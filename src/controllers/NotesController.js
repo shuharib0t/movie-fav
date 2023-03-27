3c86d6a41b7cc56644e721280551af706c877cc2
shuharib0t <shuharib0t@gmail.com>
2023-03-27 15:27:14 -0300

    adicionando a função index

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -44,6 +44,47 @@ class NotesController {
 
     return res.json();
   }
+
+  async index (req, res) {
+    const { title, user_id, rating, tags } = req.query;
+
+    let movieNotes;
+
+    if(tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim());
+      
+      movieNotes = await knex("movie_tags")
+      .select([
+        "movie_notes.id",
+        "movie_notes.title",
+        "movie_notes.user_id",
+        "movie_notes.rating",
+      ])
+      .where("movie_notes.user_id", user_id)
+      .whereLike("movie_notes.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("movie_notes", "movie_notes.id", "movie_tags.note_id")
+      .orderBy("movie_notes.title")
+
+    } else {
+      movieNotes = await knex("movie_notes")
+      .where({ user_id })
+      .whereLike("title", `%${title}%`)
+      .orderBy("title");
+    }
+
+    const userTags = await knex("movie_tags").where({ user_id });
+    const movieNotesWithTags = movieNotes.map(note => {
+      const movieNoteTags = userTags.filter(tag => tag.note_id === note.id);
+
+    return {
+      ...note,
+      tags: movieNoteTags
+    }
+    })
+
+    return res.json(movieNotesWithTags);
+  }
 }
 
 module.exports = NotesController;
