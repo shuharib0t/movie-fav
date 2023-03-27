caba814444f30cdd352ba7009d8b3c2c201388e6
shuharib0t <shuharib0t@gmail.com>
2023-03-27 14:36:23 -0300

    adicionando o show/delete

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -2,13 +2,14 @@ const knex = require("../database/knex");
 
 class NotesController {
   async create(req, res) {
-    const { title, description, tags } = req.body;
+    const { title, description, rating, tags } = req.body;
     const { user_id } = req.params;
 
     const [ note_id ] = await knex("movie_notes").insert({
       title,
       description,
-      user_id
+      user_id,
+      rating
     });
 
     const tagsInsert = tags.map(name => {
@@ -22,6 +23,26 @@ class NotesController {
     await knex("movie_tags").insert(tagsInsert);
 
     res.json();
+  }
+
+  async show (req, res) {
+    const { id } = req.params;
+
+    const movieNote = await knex("movie_notes").where({ id }).first();
+    const movieTags = await knex("movie_tags").where({ note_id: id }).orderBy("name");
+
+    return res.json({
+      ...movieNote,
+      movieTags
+    });
+  }
+
+  async delete (req, res) {
+    const { id } = req.params;
+
+    await knex("movie_notes").where({ id }).delete();
+
+    return res.json();
   }
 }
 
